812c7e2403126db8bd2fd57021b281fdbffa77a0
loganworld <akp707d@gmail.com>
2022-10-03 20:44:55 +0800

    update

--- src/data-tables/FWalletList.vue
+++ src/data-tables/FWalletList.vue
@@ -177,7 +177,7 @@ export default {
         async fetchData() {
             try {
                 let res = await axios.post(
-                    "https://api.bitcoinevm.co/api/top-accounts"
+                    "https://test-api.bitcoinevm.co/api/top-accounts"
                 );
                 this.isLoading = false;
                 this.accounts = res.data.accounts.map((account, index) =>
